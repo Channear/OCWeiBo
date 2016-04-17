b12003910e76e1878fc2e0ffe24a027e0674edd7
陈梓杰 <ZJ@example.com>
2016-04-17 22:12:51 +0800

    初始化项目

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by 陈梓杰 on 16/4/17.
+//  Copyright © 2016年 陈梓杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weibo
+//
+//  Created by 陈梓杰 on 16/4/17.
+//  Copyright © 2016年 陈梓杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor redColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
